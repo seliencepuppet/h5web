a711887c577d00fe3c212833ea65ff6edb95d123
seliencepuppet <zhangyuzeoffice@163.com>
2018-05-24 20:50:53 +0800

    Update main.js

--- nodejs/mysql/prictice01/src/middlewares/main.js
+++ nodejs/mysql/prictice01/src/middlewares/main.js
@@ -4,20 +4,20 @@ var config = require('./config.js');
 module.exports.createServer = createServer;
 
 function createServer(){
-	// 查询所有数据
-	var selectAllUser = sqlmap.selectAllUser;
-	var datas01 = [];
-	querys.selectRes(selectAllUser, datas01, function(result){
-		if(result.success == 1){
-			console.log(result.dbres.length);	
-		}
-	});
+    // 查询所有数据
+    var selectAllUser = sqlmap.selectAllUser;
+    var datas01 = [];
+    querys.selectRes(selectAllUser, datas01, function(result){
+        if(result.success == 1){
+	    console.log(result.dbres.length);	
+        }
+    });
 
-	// 查询一条数据
-	var selectUser = sqlmap.selectUser;
-	var unique_code = '12345';
-	var datas02 = [unique_code];
-	querys.selectRes(selectUser, datas02, function(result){
-		console.log(result);	
-	});
+    // 查询一条数据
+    var selectUser = sqlmap.selectUser;
+    var unique_code = '12345';
+    var datas02 = [unique_code];
+    querys.selectRes(selectUser, datas02, function(result){
+        console.log(result);	
+    });
 }
