76166595cbd96fc579e9dca683b73e6e3ee2d5d6
seliencepuppet <zhangyuzeoffice@163.com>
2018-05-24 20:56:32 +0800

    Update dbconnection.js

--- nodejs/mysql/prictice01/src/middlewares/dbconnection.js
+++ nodejs/mysql/prictice01/src/middlewares/dbconnection.js
@@ -3,34 +3,34 @@ var config = require('./config.js');
 var pool = mysql.createPool(config.hia);
 
 var queryres = {
-	selectRes: function(sql, sqlParam, callback){
-		pool.getConnection(function(err, conn){
-			var result = new Object();
-			if(err){
-				result.success = 0;
-				result.message = err.message;
-				console.log('select error:'+err.message);	
-			}else{
-				conn.query(sql, sqlParam, function(qerr, rows){
-					conn.release();
-					if(qerr){
-						result.success = 0;
-						result.message = qerr.message;
-						console.log('select error:'+qerr.message);	
-					}else{
-						if(rows.length > 0){
-							result['dbres'] = rows;
-							result.success = 1;	
-						}else{
-							console.log('no results');
-							result.success = 0;	
-						}
-					}
-					callback(result);
-				});	
-			}
-		});	
-	},
+    selectRes: function(sql, sqlParam, callback){
+	pool.getConnection(function(err, conn){
+	    var result = new Object();
+	    if(err){
+	        result.success = 0;
+	        result.message = err.message;
+	        console.log('select error:'+err.message);	
+	    }else{
+	        conn.query(sql, sqlParam, function(qerr, rows){
+	            conn.release();
+	            if(qerr){
+	                result.success = 0;
+		        result.message = qerr.message;
+		        console.log('select error:'+qerr.message);	
+	            }else{
+		        if(rows.length > 0){
+		            result['dbres'] = rows;
+		            result.success = 1;	
+		        }else{
+		            console.log('no results');
+		            result.success = 0;	
+		        }
+	            }
+	            callback(result);
+	        });	
+	    }
+        });	
+    },
 	selectOne: function(sql, sqlParam, callback){
 		pool.getConnection(function(err,conn){
 			var result = new Object();
