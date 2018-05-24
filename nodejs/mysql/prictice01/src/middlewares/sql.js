573204200bc5c4172b2569e1cfbd50608665c159
seliencepuppet <zhangyuzeoffice@163.com>
2018-05-24 20:51:17 +0800

    Update sql.js

--- nodejs/mysql/prictice01/src/middlewares/sql.js
+++ nodejs/mysql/prictice01/src/middlewares/sql.js
@@ -1,6 +1,6 @@
 var newsql = {
-	selectUser: 'select * from t_user where id = ?;',
-	selectAllUser: 'select * from t_user;'	
+    selectUser: 'select * from t_user where id = ?;',
+    selectAllUser: 'select * from t_user;'	
 }
 
 module.exports = newsql;
